b3f8b13d0b4d9749b102fa048d4e804c6117a119
James DiRosa <jdirosa@barracuda.com>
2022-02-09 09:19:33 -0500

    Refactor

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const { updateLedger } = require("./dal/leger");
 const { parseDictionary } = require("./dal/parser");
 const { runGame } = require("./services/solver");
 
-// runGame("aloof", 232, false);
+runGame("humor", 235, false, "flare");
 // parseDictionary();
